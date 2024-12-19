d98be04596da7883a960b93c6d7c9fa6def22861
@Rj <fleas2016@yahoo.com>
2024-12-19 21:28:27 +0800

    Initial commit

---
+++ src/main/java/com/tagit/commons/LoadSimulator/LoadSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.tagit.commons.LoadSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tagit/commons/LoadSimulator/LoadSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagit.commons.LoadSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
